081c921af2ac0201be6bbde41a592e19abd8234b
Chuck <bnsf5622@gmail.com>
2020-03-30 15:22:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace OOPSConcepts
+{
+    class ClassRoom
+    {
+        public void classRoomMethod()
+        {
+            Console.WriteLine("I am inside the Parent Class");
+        }
+    }
+    class child : ClassRoom
+    {
+        new public void classRoomMethod()
+        {
+            Console.WriteLine("I am insie the child class");
+        }
+    }
+    class NewClass
+    {
+        static void Main(string[] args)
+        {
+            child objChild = new child();
+            objChild.classRoomMethod();
+            Console.ReadLine();
+
+        }
+    }
+}
